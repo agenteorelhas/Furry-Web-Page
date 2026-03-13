f06e875b41e30671881fc40eea04e8ebfd0482e7
Coelho <guiotako2442@gmail.com>
2026-03-13 14:04:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -82,3 +82,35 @@ humor=0;
 }
 
 }
+
+particlesJS("particles-js", {
+
+particles: {
+
+number: {
+value: 80
+},
+
+color: {
+value: "#ffffff"
+},
+
+shape: {
+type: "circle"
+},
+
+opacity: {
+value: 0.5
+},
+
+size: {
+value: 3
+},
+
+move: {
+speed: 2
+}
+
+}
+
+});
